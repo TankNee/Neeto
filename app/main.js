94335bc5bf93d21db4859666b106c6cf4589e9d0
tanknee <nee@tanknee.cn>
2020-01-05 16:53:47 +0800

    完成拖拽，回滚，保存到本地等操作

--- app/main.js
+++ app/main.js
@@ -11,6 +11,15 @@ const windows = new Set()
 app.on('ready', () => {
     creatWindow();
 })
+app.on('will-finish-launching', () => {
+    // 外界触发的文件打开事件
+    app.on('open-file', (e, file) => {
+        const win = creatWindow()
+        win.once('ready-to-show', () => {
+            openFile(win, file)
+        })
+    })
+})
 // 在集合中创建一个新的窗口
 const creatWindow = exports.creatWindow = () => {
     let newWindow = new BrowserWindow({
@@ -30,6 +39,8 @@ const creatWindow = exports.creatWindow = () => {
         newWindow = null
         // app.quit()
     })
+    windows.add(newWindow)
+    return newWindow
 }
 
 const getFileFromUser = exports.getFileFromUser = (targetWindow) => {
@@ -48,7 +59,7 @@ const getFileFromUser = exports.getFileFromUser = (targetWindow) => {
             if (file) {
                 openFile(targetWindow, file)
             }
-            
+
         })
     }
 }
@@ -57,6 +68,59 @@ const getFileFromUser = exports.getFileFromUser = (targetWindow) => {
  */
 const openFile = exports.openFile = (targetWindow, file) => {
     const content = fs.readFileSync(file).toString()
+    app.addRecentDocument(file)
+    targetWindow.setRepresentedFilename(file)
     // 向渲染进程发送消息
     targetWindow.webContents.send('file-opened', file, content);
+}
+/**
+ * 将文本内容保存为HTML文件
+ */
+const saveHtml = exports.saveHtml = (targetWindow, content) => {
+    const file = dialog.showSaveDialog(targetWindow, {
+        title: '保存为HTML',
+        defaultPath: app.getPath('documents'),
+        filters: [
+            { name: 'HTML 文件', extensions: ['html', 'htm'] }
+        ]
+    })
+    if (file) {
+        console.log(file)
+        file.then(res => {
+            console.log(res)
+            if(res.filePath !== ''){
+                fs.writeFileSync(res.filePath, content);
+            }
+        }).catch(res =>{
+            console.log(res)
+        })
+    }
+}
+/**
+ * 保存当前的文件为markdown
+ * @param targetWindow 当前窗口
+ * @param file 当前文件的路径
+ * @param content 文本内容
+ */
+const saveMarkdown = exports.saveMarkdown = (targetWindow,file, content) => {
+    // 如果是全新的文件，那么就让用户选择存储的路径
+    if (!file) {
+        file = dialog.showSaveDialog(targetWindow,{
+            title: '保存为Markdown',
+            defaultPath: app.getPath('documents'),
+            filters: [
+                {name: 'Markdown 文件',  extensions: ['md']}
+            ]
+        })
+    }
+    // 如果是新文件，那么获取到的将会是一个对象而不是一个简单的路径，所以需要一定的处理才能获得相应的路径
+    if (!(file instanceof Object)) {
+        fs.writeFileSync(file,content)
+    }else{
+        file.then(res => {
+            console.log(res)
+            fs.writeFileSync(res.filePath, content);
+            openFile(targetWindow,res.filePath)
+        })
+    }
 }
--- app/renderer.js
+++ app/renderer.js
@@ -1,7 +1,10 @@
 const marked = require('marked')
 const { remote, ipcRenderer } = require('electron')
 const mainProcess = remote.require('./main.js')
+const path = require('path')
 
+let filePath = null;
+let originalContent = '';
 
 // 获取页面中的各个控件
 const markdownView = document.querySelector('#markdown')
@@ -20,26 +23,109 @@ const currentWindow = remote.getCurrentWindow();
 //定义一个函数用于重复执行HTML渲染的任务
 const rendererMarkDownToHtml = (markdown) => {
     htmlView.innerHTML = marked(markdown, {
-        sanitize: true
+        sanitize: false
     })
 }
 
 // 为输入框绑定事件
 markdownView.addEventListener('keyup', (e) => {
     const content = e.target.value
-    console.log(content)
     rendererMarkDownToHtml(content)
+    updateUserInterface(content !== originalContent);
 })
+
 // 打开文件
 openFileButton.addEventListener('click', () => {
     mainProcess.getFileFromUser(currentWindow);
 })
+
 // 监听频道，接收主进程传递来的消息
 ipcRenderer.on('file-opened', (e, file, content) => {
+    filePath = file
+    originalContent = content
     markdownView.value = content;
     rendererMarkDownToHtml(content);
+    updateUserInterface();
 })
+
 // 新建文件
-newFileButton.addEventListener('click',()=>{
+newFileButton.addEventListener('click', () => {
     mainProcess.creatWindow();
+})
+
+// 基于打开的文件更新窗口标题
+const updateUserInterface = (isEdited) => {
+    let title = 'Neeto'
+    if (filePath) {
+        // 如果有文件被打开，那么就将文件名放在标题的前面
+        title = `${path.basename(filePath)} - ${title}`
+    }
+    if (isEdited) {
+        title = `${title} (Edited)`;
+    }
+    currentWindow.setTitle(title)
+    if (process.platform == 'darwin') {
+        currentWindow.setDocumentEdited(isEdited)
+    }
+
+
+    saveMarkdownButton.disabled = !isEdited
+    // saveHtmlButton.disabled = !isEdited
+}
+
+// 将html内容保存为文件
+saveHtmlButton.addEventListener('click', () => {
+    mainProcess.saveHtml(currentWindow, htmlView.innerHTML)
+})
+
+// 将markdown文件保存下来
+saveMarkdownButton.addEventListener('click', () => {
+    originalContent = markdownView.value
+    mainProcess.saveMarkdown(currentWindow, filePath, markdownView.value)
+})
+
+// 回滚
+revertButton.addEventListener('click', () => {
+    markdownView.value = originalContent
+    rendererMarkDownToHtml(originalContent)
+})
+
+// 拖拽文件
+document.addEventListener('dragstart', e => e.preventDefault());
+document.addEventListener('dragover', e => e.preventDefault());
+document.addEventListener('dragleave', e => e.preventDefault());
+document.addEventListener('drop', e => e.preventDefault());
+
+const getDraggedFile = (e) => {
+    return e.dataTransfer.items[0]
+}
+const getDroppedFile = (e) => {
+    return e.dataTransfer.files[0]
+}
+const fileTypeIsSupported = (file) => {
+    console.log(file)
+    return ['text/plain','text/x-markdown','text/md','image/png','image/jpeg','image/jpg',''].includes(file.type)
+}
+markdownView.addEventListener('dragover', (e) => {
+    const file = getDraggedFile(e)
+    if (fileTypeIsSupported(file)) {
+        markdownView.classList.add('drag-over')
+    } else {
+        markdownView.classList.add('drag-error')
+    }
+})
+markdownView.addEventListener('drop',(e)=>{
+    const file = getDroppedFile(e)
+    if (fileTypeIsSupported(file)) {
+        mainProcess.openFile(currentWindow,file.path)
+    } else {
+        alert("这种文件暂时不支持编辑")
+    }
+    markdownView.classList.remove('drag-over')
+    markdownView.classList.remove('drag-error')
+})
+markdownView.addEventListener('dragleave', () => {
+    // 清除样式
+    markdownView.classList.remove('drag-over')
+    markdownView.classList.remove('drag-error')
 })
